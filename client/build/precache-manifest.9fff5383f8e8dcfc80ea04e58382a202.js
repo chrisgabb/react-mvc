119e6650d6034c16e3acf56ecb33c6dc2cc3d74d
bkny1020 <cgabb1020@gmail.com>
2020-12-07 20:00:58 -0500

    first commit

---
+++ client/build/precache-manifest.9fff5383f8e8dcfc80ea04e58382a202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2673e117c56c4d84b42737142c3e4cac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "780783ef1b39b2ece1c4",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "5309e449abb858676493",
+    "url": "/static/js/2.7f4868fe.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.7f4868fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "780783ef1b39b2ece1c4",
+    "url": "/static/js/main.ac2ea991.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
